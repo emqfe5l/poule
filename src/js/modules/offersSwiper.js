71cac5e4c837dd1d94e2f2badc2652a6ed665267
AJ <yaroslav01593@gmail.com>
2018-09-22 00:21:01 +0300

    mobile finished

--- src/js/modules/offersSwiper.js
+++ src/js/modules/offersSwiper.js
@@ -4,8 +4,17 @@ const offersSwiper = (() => {
     const buildSwiper = () => {
         mySwiper = new Swiper(holder, {
             loop: true,
+            spaceBetween: 10,
             pagination: {
                 el: '.swiper-pagination',
+            },
+            breakpoints: {
+                769: {
+                    slidesPerView: 2
+                },
+                415: {
+                    slidesPerView: 1
+                }
             }
         });
     };
@@ -14,12 +23,12 @@ const offersSwiper = (() => {
         mySwiper.destroy();
     };
     const init = () => {
-        if(!holder) {
+        if (!holder) {
             return;
         }
-        enquire.register("(max-width: 768px)", {
-            match : () => buildSwiper(),            
-            unmatch : () => destroySwiper(),            
+        enquire.register("(max-width: 767px)", {
+            match: () => buildSwiper(),
+            unmatch: () => destroySwiper(),
         });
     };
 
