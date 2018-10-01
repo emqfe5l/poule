e9d54e07f95317d76bb1fa5cecfece3f0c20919f
zabellfour <zabell2814@gmail.com>
2018-10-02 01:32:14 +0300

    added 04 page

--- src/js/modules/innerNavigation.js
+++ src/js/modules/innerNavigation.js
@@ -1,15 +1,30 @@
 const innerNav = (() => {
     const init = () => {
         let $innerNavToggle = $('.has-inner-menu');
-        if(!$innerNavToggle) {
+        if (!$innerNavToggle) {
             return;
         }
         $innerNavToggle.click(function() {
             let $innerNav = $(this).next('.inner-nav');
             console.log($innerNav);
-            
+
             $innerNav.toggleClass('is-active');
         });
+        if ($(window).width() < 768) {
+            let $typeCategory = $('.type-category');
+
+            if (!$typeCategory) {
+                return;
+            }
+            $typeCategory.click(function() {
+                $(this).toggleClass('visible-menu');
+                console.log(11)
+            });
+        } else {
+            $typeCategory.click(function() {
+                $(this).removeClass('visible-menu');
+            });
+        }
     };
 
     return {
